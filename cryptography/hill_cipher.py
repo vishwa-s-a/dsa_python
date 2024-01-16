29f9b57e3ab2e85e336f7a86f65c4dc9d954ac5d
vishwa-s-a <vishwasappaji769@gmail.com>
2024-01-16 11:33:17 +0530

    add decrypt part for hill cipher

--- cryptography/hill_cipher.py
+++ cryptography/hill_cipher.py
@@ -1,3 +1,90 @@
+
+def get_cofactor(mat,p,q,temp,n):
+    #here mat is original matrix,temp will get the cofactor matrix,n is the size of the received matrix
+    #here temp will store the co-factor matrix which will be of size k-1
+    i=0
+    j=0
+    for row in range(n):
+        for col in range(n):
+            if(row!=p and col!=q):
+                temp[i][j]=mat[row][col]
+                j+=1
+                if(j==n-1):
+                    j=0
+                    i+=1
+    # here the temp will contain the cofactor matrix and we are not returning anything as we are directly modifing the temp variable
+
+def determinant(mat,n):#n will be the size of the matrix for which det to be calculated (varies, as it is a recurssive function)
+    #initialize the final answer value
+    D=0
+    if(n==1):
+        return mat[0][0]
+    temp=[]#to store the co-factor matrix
+    for i in range(k):
+        temp.append([None for v in range(k)])#initializing a matrix with None values to store the co-factor matrix
+    
+    sign=1
+    for f in range(n):
+        get_cofactor(mat,0,f,temp,n)
+        D+=sign*mat[0][f]*determinant(temp,n-1)
+
+        sign=-sign
+    return D
+    
+    
+def adjoint_mat(mat,adj):
+    if(k==1):
+        adj[0][0]=1
+        return 
+    
+    sign=1
+    temp=[]# to store the cofactors
+    for i in range(k):
+        temp.append([None for _ in range(k)])# here we use _ as the iterating variable generally i,j is not used inside the loop so we use _
+    
+    for i in range(k):
+        for j in range(k):
+            get_cofactor(mat,i,j,temp,k)
+
+            # sign of adj[j][i] positive if sum of row
+            # and column indexes is even.
+            if((i+j)%2==0):
+                sign=1
+            else:
+                sign=-1
+            
+            adj[j][i]=(sign)*(determinant(temp,k-1))
+#function to find the inverse of the matrix
+def find_multplicative_inverse(num):
+    for i in range(500):
+        if((i*num)%26==1):
+            return i
+def find_inverse(matrix,k):
+    ## first we will find the determinant of the matrix
+    det=determinant(matrix,k)
+    det=det%26
+
+    adj=[]
+    for i in range(k):
+        adj.append([None for _ in range(k)])#here i or j can be used in place of _
+    adjoint_mat(matrix,adj)
+
+    #we will copy adj to adjoint for further mod operations
+    adjoint=list.copy(adj)
+    for i in range(k):
+        for j in range(k):
+            adjoint[i][j]=adjoint[i][j]%26
+    new_det=find_multplicative_inverse(det)
+
+    for i in range(k):
+        for j in range(k):
+            adjoint[i][j]=(new_det*adjoint[i][j])%26
+    
+    # print(new_det)
+    # print(adjoint)
+
+    return adjoint
+
 def encrypt(matrix,k,polygrams):
     answer=list()
     list2=list()
@@ -22,8 +109,11 @@ def encrypt(matrix,k,polygrams):
             print(j,end='')
     print('\r')
 
-def decrypt():
-    pass
+
+def decrypt(matrix,k,polygrams):
+    print(polygrams)
+    inverse_mat=find_inverse(matrix,k)
+    encrypt(inverse_mat,k,polygrams)
 
 
 choice=int(input('Enter 0 to encrypt\tEnter 1 to decrypt\n'))
@@ -33,6 +123,7 @@ if(choice==0):
 else:
     t=input('Enter the text to decrypt: ')
     txt=t.replace(' ','',-1)
+
 k=int(input('Enter the size of the key: '))
 matrix=list()
 temp=list()
@@ -57,7 +148,10 @@ while(count<len(txt)):
         z=0
         polygrams.append(list.copy(list1))
         list1.clear()
-print("Encrypted text is: ")
-encrypt(matrix,k,polygrams)
-
+if(choice==0):
+    print("Encrypted text is: ")
+    encrypt(matrix,k,polygrams)
+else:
+    print("Decrypted text is: ")
+    decrypt(matrix,k,polygrams)
     
--- temp.py
+++ temp.py
@@ -1,25 +1,4 @@
-# Create an empty 2D list (matrix)
-matrix = []
-
-# Define the dimensions of the matrix (rows and columns)
-rows = 3
-columns = 4
-
-# Initialize the matrix with zeros or any default value
-for _ in range(rows):
-    matrix.append([0] * columns)
-
-# Display the initial matrix
-print("Initial Matrix:")
-for row in matrix:
-    print(row)
-
-# Add values to the matrix
-for i in range(rows):
-    for j in range(columns):
-        matrix[i][j] = i * columns + j + 1
-
-# Display the matrix after adding values
-print("\nMatrix After Adding Values:")
-for row in matrix:
-    print(row)
+temp=list()#to store the co-factor matrix
+for i in range(3):
+    temp.append([None for _ in range(3)])
+print((temp))
